1983f8e199aea7c4254ab86ccce16866a56b8936
WessCardZ <wesleyofficial@hotmail.com>
2024-01-17 11:58:39 -0300

    feat: permitir ao front de receber e enviar a mensagem com Socket IO

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.json());
 io.on('connection', function (socket) {
     console.log('Usuario conectado com o id:', socket.id);
 
+    socket.on('enviarMensagem', function (msg) {
+        io.emit('receberMensagem', { id: socket.id, mensagem: msg })
+    })
+
+    socket.on('audio', function (base64Data) {
+        io.emit('receberMensagem', { id: socket.id, base64Data })
+    })
+
     socket.on('disconnect', function () {
         console.log('Usuario desconectado!');
     })
